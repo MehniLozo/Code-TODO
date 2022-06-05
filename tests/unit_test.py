8c11578b4514f748122238ae265a13be0a23f906
mehni <lozomehni28@gmail.com>
2022-06-05 13:19:04 +0100

    some testing to be dne

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,10 +1,11 @@
 
 from typer.testing import CliRunner
-
+import json
+import pytest
 #Typer testing integrates well with pytest
 #CliRunner is a class that allows to create a runner for testing responses to real-worlds commands
 
-from todo import __app_name__, __version__, cli
+from todo import __app_name__, __version__, cli, DB_READ_ERROR,SUCCESS,todo
 
 
 runner = CliRunner()
@@ -17,3 +18,34 @@ def test_version():
     assert result.exit_code == 0
 
     assert f"{__app_name__} v{__version__}\n" in result.stdout
+
+@pytest.fixture
+def dummy_json(tmp_path):
+    todo = [{"Description": "Write API.", "Priority": 1, "Done": False}]
+    db_file = tmp_path / "todo.json"
+    with db_file.open("w") as db_data:
+        json.dump(todo,db_data,ident=4)
+	
+    return db_file
+
+
+
+
+data_test1 = {
+    "description": ["Build", "the", "API"],
+    "priority": 1,
+    "todo": {
+        "Description": "Build the API.",
+        "Priority": 1,
+        "Done": False,
+    },
+}
+data_test2 = {
+    "description": ["Implement Test"],
+    "priority": 2,
+    "todo": {
+        "Description": "Implement Test.",
+        "Priority": 2,
+        "Done": False,
+    },
+}
